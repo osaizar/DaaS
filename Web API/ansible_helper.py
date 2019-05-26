dfb772093aef9abdf66e70b829371340c8720168
osaizar <oierlas@gmail.com>
2019-05-26 21:03:51 +0200

    ansible

--- Web API/ansible_helper.py
+++ Web API/ansible_helper.py
@@ -4,11 +4,13 @@ import os
 import subprocess
 import re
 
-IP_RE ="ip_addr(.*)ip_addr"
+IP_RE = r"ip_addr(.*)ip_addr"
+PREV_DIR = ""
+
 
 # DO ANSIBLE THINGS
 def generate_userlist(characters, campaign):
-	with open('csv/'+campaign.name+'_users.csv', 'w') as f: # TODO: dynamic name
+	with open(PREV_DIR+'csv/'+campaign.name+'_users.csv', 'w') as f: # TODO: dynamic name
 		for i, c in enumerate(characters):
 			passwd = db.get_user_by_id(c.user).password
 			ch_class = db.get_character_class_by_id(c.ch_class).name
@@ -16,18 +18,18 @@ def generate_userlist(characters, campaign):
 
 	
 def generate_grouplist():
-	with open('csv/group_list.csv', 'w') as f:
+	with open(PREV_DIR+'csv/group_list.csv', 'w') as f:
 		for i,c in enumerate(CLASSES):
 			f.write(c+","+str(i+2001)+"\n")
 
 
 def generate_admin_pass(admin):
-	with open('csv/mastercreds.csv', 'w') as f:
+	with open(PREV_DIR+'csv/mastercreds.csv', 'w') as f:
 		f.write(admin.password)
 
 
 def add_ip_to_inventory(ip):
-	inv = open('ansible/inventory', 'r').read()
+	inv = open(PREV_DIR+'ansible/inventory', 'r').read()
 
 	new_inv = ""
 	for l in inv.split("\n"):
@@ -35,17 +37,20 @@ def add_ip_to_inventory(ip):
 		if "[instances]" in l:
 			new_inv += str(ip)+"\n"
 
+	with open(PREV_DIR+'ansible/inventory', 'w') as f:
+		f.write(new_inv)
+
 
 def start_instance():
-	out = subprocess.getoutput("ansible-playbook -i ansible/inventory ansible/despliegue.yml")
-	instance_ip = re.findall(r"ipadr(.*)ipadr", out)[0]
+	out = subprocess.getoutput("ansible-playbook -i "+PREV_DIR+"ansible/inventory "+PREV_DIR+"ansible/despliegue.yml")
+	instance_ip = re.findall(IP_RE, out)[0]
 
 	add_ip_to_inventory(instance_ip)
 
-	subprocess.getoutput("ansible-playbook -i ansible/inventory --limit "+instance_ip+" ansible/create_class_groups.yml")
-	subprocess.getoutput("ansible-playbook -i ansible/inventory --limit "+instance_ip+" ansible/create_level_groups.yml")
-	subprocess.getoutput("ansible-playbook -i ansible/inventory --limit "+instance_ip+" ansible/create_user_fromfile.yml")
-	subprocess.getoutput("ansible-playbook -i ansible/inventory --limit "+instance_ip+" ansible/setpermissionss_bash.yml")
-	subprocess.getoutput("ansible-playbook -i ansible/inventory --limit "+instance_ip+" ansible/reset_root_pwd.yml")
+	print(subprocess.getoutput("ansible-playbook -i "+PREV_DIR+"ansible/inventory --limit "+instance_ip+" "+PREV_DIR+"ansible/create_class_groups.yml"))
+	print(subprocess.getoutput("ansible-playbook -i "+PREV_DIR+"ansible/inventory --limit "+instance_ip+" "+PREV_DIR+"ansible/create_level_groups.yml"))
+	print(subprocess.getoutput("ansible-playbook -i "+PREV_DIR+"ansible/inventory --limit "+instance_ip+" "+PREV_DIR+"ansible/create_user_fromfile.yml"))
+	print(subprocess.getoutput("ansible-playbook -i "+PREV_DIR+"ansible/inventory --limit "+instance_ip+" "+PREV_DIR+"ansible/setpermissionss_bash.yml"))
+	print(subprocess.getoutput("ansible-playbook -i "+PREV_DIR+"ansible/inventory --limit "+instance_ip+" "+PREV_DIR+"ansible/reset_root_pwd.yml"))
 
 	return instance_ip
--- Web API/server.py
+++ Web API/server.py
@@ -26,8 +26,7 @@ def index():
     <h3>Allowed methods:</h3>
     <p>/sign_up {"username", "password"}</p>
     <p>/login {"username", "password"}</p>
-    <p>/logout </p
-    <p>/sign_up {"username", "password"}</p>
+    <p>/logout </p>
     <p>/create_character {"name", "character_class", "lvl"}</p>
     <p>/create_campaign {"name", "characters"}</p>
     """
@@ -133,6 +132,7 @@ def create_campaign():
         for c in characters:
             db.add(CampaignCharacter(c.id, campaign.id))
 
+        ansible.generate_grouplist()
         ansible.generate_userlist(characters, campaign)
         ansible.generate_admin_pass(user)
         instance_ip = ansible.start_instance()
