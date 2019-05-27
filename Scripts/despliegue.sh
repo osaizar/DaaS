0023c7cadee7bf18caab5ec16e48b6819ef5cdeb
osaizar <oierlas@gmail.com>
2019-05-27 04:10:07 +0200

    deployment script

--- Scripts/despliegue.sh
+++ Scripts/despliegue.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+source /opt/stack/devstack/openrc
+inst_name="inst_$RANDOM"
 
-source /root/keystonerc_admin
+openstack server create --flavor DandD --image ubuntu --nic net-id=c45da0d4-ec13-4607-a416-82c1fa19a8bd $inst_name > /dev/null
 
-openstack server create --flavor m1.tiny --image cirros --nic net-id=62bc9b97-24fb-46d6-a484-afabbd2806b9 instance
-
-neutron floatingip-create public -f shell > /root/floating_ip.txt
-floating_ip="$(grep floating_ip_address /root/floating_ip.txt)"
-nova floating-ip-associate instance ${floating_ip:21:-1}
-echo "$floating_ip" >> /root/ip_asociadas.txt
+openstack floating ip create public > /tmp/floating_ip.txt
+floating_ip="$(grep floating_ip_address /tmp/floating_ip.txt)"
+openstack server add floating ip $inst_name ${floating_ip:24:-1}
+echo "${floating_ip:24:-1}"
+rm /tmp/floating_ip.txt
